ef7e0f096b7e1b022f42ee44e6261c88744cf827
Terrence <121534028+tblu4982@users.noreply.github.com>
2024-05-30 16:50:23 -0400

    Add files via upload

--- separator.py
+++ separator.py
@@ -13,6 +13,7 @@ file_path = askopenfilename(title = 'Select CSV File', filetypes = [('CSV Files'
 advisor_query = "Mohammed, Ahmed Farouk|Shelton, Joseph|Davis, Brittany|Wang, Ju|REYNOLDS, MICHAEL|Chen, Wei-Bang|Haris Rais, Muhammad"
 #list to hold column titles from csv
 columns = []
+columns_found = False
 #list to hold students who have no advisor
 no_advisor = []
 #lists to hold students by classification
@@ -23,35 +24,38 @@ sr = []
 
 #open csv and convert it into a 3d array
 with open(file_path, 'r') as myfile:
-    for line in myfile:
-        csvreader = csv.reader(myfile)
-        if re.search("Student Alternate ID", line):
-            columns = line.split(",")
-            continue
-        try:
-            has_advisor = False
-            #find the student's classification and store it in the appropriate list
-            for row in csvreader:
-                for index in row:
-                    if re.search("Freshman|Sophomore|Junior|Senior", index):
-                        if re.search("Freshman", index):
-                            fr.append(row)
-                        elif re.search("Sophomore", index):
-                            so.append(row)
-                        elif re.search("Junior", index):
-                            jr.append(row)
-                        elif re.search("Senior", index):
-                            sr.append(row)
-                    #checks to see if student has a CS advisor
-                    if re.search(advisor_query, index):
-                        has_advisor = True
-                #if student doesn't have CS advisor, put them in the no advisors list
-                if not has_advisor:
-                    no_advisor.append(row)
-        except IndexError:
-            continue
+    csvreader = csv.reader(myfile)
+    #find the student's classification and store it in the appropriate list
+    for row in csvreader:
+        has_advisor = False
+
+        for index in row:
+            if not columns_found:
+                if re.search("Student Alternate ID", index):
+                    columns = row
+                    columns_found = True
+                    continue
+            elif re.search("Freshman|Sophomore|Junior|Senior", index):
+                if re.search("Freshman", index):
+                    fr.append(row)
+                elif re.search("Sophomore", index):
+                    so.append(row)
+                elif re.search("Junior", index):
+                    jr.append(row)
+                elif re.search("Senior", index):
+                    sr.append(row)
+                else:
+                    continue
+            #checks to see if student has a CS advisor
+            if re.search(advisor_query, index):
+                has_advisor = True
+        #if student doesn't have CS advisor, put them in the no advisors list
+        if not has_advisor and columns_found:
+            no_advisor.append(row)
 
 #prepare lists to a dataframe
+for c in no_advisor:
+    print(c)
 df1 = pd.DataFrame(fr, columns = columns)
 df2 = pd.DataFrame(so, columns = columns)
 df3 = pd.DataFrame(jr, columns = columns)
